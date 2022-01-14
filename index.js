2ec9f8fbbc9da3ea4b65c1d068549a358a840984
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-13 19:52:24 -0700

    commit tree 067a87243ccb96aab668c5d10805d196b1aee480

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  const result = drivers.filter(
+    (element) => element === string || element === string.toLowerCase()
+  );
+  return result;
+}
+function fuzzyMatch(driversName, string) {
+  const nameFirstLetter = driversName.filter(
+    (element) => element.slice(0, string.length) === string
+  );
+  return nameFirstLetter;
+}
+
+function matchName(driverObjects, aString) {
+  const matchingDrivers = driverObjects.filter(
+    (driver) => driver.name === aString
+  );
+  return matchingDrivers;
+}
